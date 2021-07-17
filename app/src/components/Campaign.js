ae66450043844045b1ddcd7347e2a39be3689f59
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-07-17 15:52:34 -0700

    templated layout of project

---
+++ app/src/components/Campaign.js
@@ -0,0 +1,49 @@
+function Campaign() {
+    return (
+        <div>
+            {/* ======== Campaign Heading ===================== */}
+            <div>
+
+                {/* ==== Campaign Icon ================ */}
+                <div>
+
+                </div>
+                
+                {/* ====== Campaign Name =========== */}
+                <div>
+
+                </div>
+
+                {/* ===== Campaign Pay Per Install */}
+                <div>
+
+                </div>
+            </div>
+
+            {/* ====== Campaign Scrollable Media List ================== */}
+            <div>
+
+                {/* ============== Campaign Media Cover Photo ============== */}
+                <div>
+
+                </div>
+
+                {/* ====== Campaign Media Copy Button =========== */}
+                <div>
+
+                </div>
+
+                {/* ======== Campaign Media Download Button =========== */}
+                <div>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Campaign
